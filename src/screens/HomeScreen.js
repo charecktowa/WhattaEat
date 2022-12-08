bd4f30174882d509027575c0de0f0ca407af3629
Alfredo Gutiérrez Alfaro <charecktowa@protonmail.com>
2022-12-07 18:15:14 -0600

    Menu

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,46 +1,83 @@
 import axios from "axios";
 import React, { useContext, useState, useEffect } from "react";
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AuthContext } from "../context/AuthContext";
 
 const HomeScreen = () => {
-    const { userInfo, logout, isLoading } = useContext(AuthContext);
-    let { sites } = useState(null)
+    const { userInfo } = useContext(AuthContext);
 
+    const [data, setData] = useState([]);
+    const [isLoading, setLoading] = useState(false);
 
-    const fetchImages = (userInfo) => {
+    useEffect(() => {
+        setLoading(true);
 
-        axios.get(`https://api.randomlearning.fun/places/places/?skip=0&limit=3`, {
+        axios.get('https://api.randomlearning.fun/places/places/?skip=0&limit=6', {
             headers: {
                 Authorization: `Bearer ${userInfo}`
             }
         }).then((res) => {
-            sites = res.data
+            console.log(res.photo)
+
+        }).then((resJSON) => {
+            setData(resJSON.results);
+            setLoading(false);
         }).catch((err) => {
             console.log(err);
-        });
+        })
+    }, []);
+
+    const renderUser = (item) => {
+        return (
+            <View style={styles.item}>
+                <Image
+                    style={styles.image}
+                    source={{ uri: item.photo }}
+                    resizeMode="contain"
+                />
+                <View style={styles.wrapText}>
+                    <Text>Hey</Text>
+                </View>
+            </View>
+        );
     }
 
     return (
-        <View>
-            <Text>Hello, WOrld</Text>
-            <Button
-                title="press me"
-                onPress={() => fetchImages(userInfo)}
-            />
-
-        </View>
+        <SafeAreaView style={styles.container}>
+            {
+                isLoading ? <ActivityIndicator /> : (
+                    <FlatList
+                        data={data}
+                        keyExtractor={item => `key-${item.id}`}
+                        renderItem={renderUser}
+                    />
+                )
+            }
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    fontSize: {
+        fontSize: 12,
     },
-    list: {
+    image: {
+        width: 100,
+        height: 100,
+    },
+    wrapText: {
         flex: 1,
-        padding: 8,
+        marginLeft: 10,
+        justifyContent: 'center',
+    },
+    item: {
+        flexDirection: 'row',
+        marginBottom: 20,
+        borderRadius: 10,
+    },
+    container: {
+        flex: 1
     },
 });
 
